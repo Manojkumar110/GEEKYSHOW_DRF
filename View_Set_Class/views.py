f6488698ffea561567adb01de655034320d0efae
“Manoj <manoj@rvj.net>
2023-04-21 11:19:54 +0530

    'Pdf_Generate'

---
+++ View_Set_Class/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+'''
+ViewSet Class:-
+1. we can use router
+2. it is not provite get(), post() and instead provide actions
+    sach as list(), create()
+
+    (a). list():- Get All Record
+    (b). retrieve():-Get Single Record
+    (c). update():-Create/Insert Record
+    (d). partial-update():- update record partial
+    (e). destroy()
+
+'''
